2687f98c96ffc0188a34c8ad19c42133e1a526ca
TzurZ <tziv@live.com>
2017-01-20 15:39:08 +0200

    added collision logic

--- utils/collision.js
+++ utils/collision.js
@@ -7,4 +7,14 @@ class Collision {
     constructor() {
         this[_wrapper] = new wrapper.SinWrapper();
     }
+
+    getReturnAngle(enterAngle, formerMedium, newMedium) {
+        //Snell's law: sin(enterAngle) * formerMedium = sin(exitAngle) * newMedium
+        let formerMediumRefractIndex = formerMedium.refractIndex;
+        let newMediumRefractIndex = newMedium.refractIndex;
+        let sinEntryAngle = this[_wrapper].sin(enterAngle);
+        let sinExitAngle = sinEntryAngle * formerMediumRefractIndex / newMediumRefractIndex;
+        let returnAngle = this[_wrapper].arcSin(sinExitAngle);
+        return returnAngle;
+    }
 }
